4bbce2c16a8d094d523170e1e4e6669bf22605d7
Ryan Menghani <menghaniryan@gmail.com>
2020-07-16 15:47:35 -0500

    coronavirus 2: electric boogaloo

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -5,19 +5,54 @@ package com.codechallenge;
  */
 public class HelloWorld {
 	public static void main(String[] args) {
-		H h = new H("H");
-		E e = new E("E");
-		L l = new L("L");
-		O o = new O('O');
-		W w = new W("W");
-		R r = new R("R");
-		D d = new D();
 
-		String hello = h.toString() + e.toString() + l.toString() + l.toString() + o.toString();
-		String world = w.toString() + o.toString() + r.toString() + l.toString() + d.toString();
+		String aksjndf = "";
+		String corona = "";
+		H h = null;
 
-		String helloWorld = hello + " " + world;
+		// ho
+		for (int i = 0; i < 1; ++i) {
+			h = new H("h");
+			aksjndf += h.toString();
+		}
 
-		System.out.println(helloWorld);
+		E e = null;
+		for (int i = 0; i < 1; ++i) {
+			e = new E("E");
+		}
+		L l = null;
+		for (int i = 0; i < 1; ++i) {
+			l = new L("L");
+		}
+		O o = null;
+		for (int i = 0; i < 1; ++i) {
+			o = new O('O');
+		}
+
+		W w = null;
+		for (int i = 0; i < 1; ++i) {
+			w = new W("W");
+			aksjndf += w.toString();
+		}
+
+		R r = null;
+		for (int i = 0; i < 1; ++i) {
+			r = new R("R");
+			corona += r.toString();
+		}
+
+		D d = null;
+		int i = 100;
+		while (i < 101) {
+			d = new D();
+			i++;
+		}
+
+		aksjndf = aksjndf.substring(0, 1).toUpperCase() + e.toString().toLowerCase() + l.toString().toLowerCase() + l.toString().toLowerCase() + aksjndf.substring(1, 2).toLowerCase();
+		corona = o.toString().toUpperCase() + w.toString().toLowerCase() + corona.substring(0, 1).toLowerCase() + l.toString().toLowerCase() + d.toString().toLowerCase();
+
+		String virus = aksjndf + " " + corona;
+
+		System.out.println(virus);
 	}
 }
