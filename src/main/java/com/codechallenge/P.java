cb8470fe928a7bf9594d3b71853d0cf8a0f2c0de
Dawson Fields <dawson@gofields.com>
2020-07-16 15:54:05 -0500

    Howdy

--- src/main/java/com/codechallenge/P.java
+++ src/main/java/com/codechallenge/P.java
@@ -1,15 +1,13 @@
 package com.codechallenge;
 
 public class P extends Thread{
-	String M;
-	String B;
-	public P(String m, String b){
-		M = m;
-		B = b;
-	}
+String M;
+String B;
+public P(String m, String b){
+M = m;
+B = b;
+}
 
-	public void run(){
-		System.out.print( (char)(int)(Integer.parseInt(Bc.bC(M, Integer.valueOf(B), 10))));
-	}
+public void run(){System.out.print( (char)(int)(Integer.parseInt(Bc.bC(M, Integer.valueOf(B), 10))));}
 
 }
