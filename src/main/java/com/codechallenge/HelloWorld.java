01eb2b8b96a6b8fe9321376af8a91462a24a2047
ealara3 <ealara3@miners.utep.edu>
2020-07-16 15:05:27 -0600

    Wolfskill

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -1,11 +1,234 @@
 package com.codechallenge;
 
+
+
 /**
  * Created by merciersj on 8/29/2018.
+ * WOLFSKILL
  */
 public class HelloWorld {
+	public static void method(){
+		NEWCLASS firstLetter = new NEWCLASS("H");
+		NEWCLASS2 firstLetter1 = new NEWCLASS2("E");
+		NEWCLASS3 firstLetter2 = new NEWCLASS3("L");
+		NEWCLASS4 firstLetter3 = new NEWCLASS4("L");
+		NEWCLASS5 firstLetter4 = new NEWCLASS5("O");
+		NEWCLASS6 firstLetter5 = new NEWCLASS6(" ");
+		NEWCLASS7 firstLetter6 = new NEWCLASS7("W");
+		NEWCLASS8 firstLetter7 = new NEWCLASS8("O");
+		NEWCLASS9 firstLetter8 = new NEWCLASS9("R");
+		NEWCLASS10 firstLetter9 = new NEWCLASS10("L");
+		NEWCLASS11 firstLetter10 = new NEWCLASS11("D");
+		NEWCLASS12 firstLetter11 = new NEWCLASS12("!");
+		System.out.println(firstLetter.EICH+""+firstLetter1.EEEE +""+firstLetter2.ELL1+""+firstLetter3.ELL2+""+firstLetter4.OOOO+""+firstLetter5.____+""+firstLetter6.WWWW+""+firstLetter7.OOO2+""+firstLetter8.RRRR+""+firstLetter9.LLLL+""+firstLetter10.DDDD+""+firstLetter11.ADMIRATION);
+	}
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		//System.out.println("Hello World");
+		method();
+
+
+
 
 	}
+}
+
+class NEWCLASS{
+	String EICH;
+	public NEWCLASS(String EICH){
+		this.EICH = EICH;
+	}
+
+	public String getEICH() {
+		return EICH;
+	}
+
+	public void setEICH(String EICH) {
+		this.EICH = EICH;
+	}
+
+
+}
+class NEWCLASS2{
+	String EEEE;
+	public NEWCLASS2(String EEEE){
+		this.EEEE = EEEE;
+	}
+
+	public String getEEEE() {
+		return EEEE;
+	}
+
+	public void setEEEE(String EEEE) {
+		this.EEEE = EEEE;
+	}
+
+
+}
+class NEWCLASS3{
+	String ELL1;
+	public NEWCLASS3(String ELL1){
+		this.ELL1 = ELL1;
+	}
+
+	public String getELL1() {
+		return ELL1;
+	}
+
+	public void setELL1(String ELL1) {
+		this.ELL1 = ELL1;
+	}
+
+
+}
+
+class NEWCLASS4{
+	String ELL2;
+	public NEWCLASS4(String ELL2){
+		this.ELL2 = ELL2;
+	}
+
+	public String getELL2() {
+		return ELL2;
+	}
+
+	public void setELL2(String ELL2) {
+		this.ELL2 = ELL2;
+	}
+
+
+}
+
+class NEWCLASS5{
+	String OOOO;
+	public NEWCLASS5(String OOOO){
+		this.OOOO = OOOO;
+	}
+
+	public String getOOOO() {
+		return OOOO;
+	}
+
+	public void setOOOO(String OOOO) {
+		this.OOOO = OOOO;
+	}
+
+
+}
+
+class NEWCLASS6{
+	String ____;
+	public NEWCLASS6(String ____){
+		this.____ = ____;
+	}
+
+	public String get____() {
+		return ____;
+	}
+
+	public void set____(String ____) {
+		this.____ = ____;
+	}
+
+
+}
+
+class NEWCLASS7{
+	String WWWW;
+	public NEWCLASS7(String WWWW){
+		this.WWWW = WWWW;
+	}
+
+	public String getWWWW() {
+		return WWWW;
+	}
+
+	public void setWWWW(String WWWW) {
+		this.WWWW = WWWW;
+	}
+
+
+}
+
+class NEWCLASS8{
+	String OOO2;
+	public NEWCLASS8(String OOO2){
+		this.OOO2 = OOO2;
+	}
+
+	public String getOOO2() {
+		return OOO2;
+	}
+
+	public void setOOO2(String OOO2) {
+		this.OOO2 = OOO2;
+	}
+
+
+}
+
+class NEWCLASS9{
+	String RRRR;
+	public NEWCLASS9(String RRRR){
+		this.RRRR = RRRR;
+	}
+
+	public String getRRRR() {
+		return RRRR;
+	}
+
+	public void setRRRR(String RRRR) {
+		this.RRRR = RRRR;
+	}
+
+
+}
+class NEWCLASS10{
+	String LLLL;
+	public NEWCLASS10(String LLLL){
+		this.LLLL = LLLL;
+	}
+
+	public String getLLLL() {
+		return LLLL;
+	}
+
+	public void setLLLL(String LLLL) {
+		this.LLLL = LLLL;
+	}
+
+
+}
+
+class NEWCLASS11{
+	String DDDD;
+	public NEWCLASS11(String DDDD){
+		this.DDDD = DDDD;
+	}
+
+	public String getDDDD() {
+		return DDDD;
+	}
+
+	public void setDDDD(String DDDD) {
+		this.DDDD = DDDD;
+	}
+
+
+}
+
+class NEWCLASS12{
+	String ADMIRATION;
+	public NEWCLASS12(String ADMIRATION){
+		this.ADMIRATION = ADMIRATION;
+	}
+
+	public String getADMIRATION() {
+		return ADMIRATION;
+	}
+
+	public void setEICH(String ADMIRATION) {
+		this.ADMIRATION = ADMIRATION;
+	}
+
+
 }
