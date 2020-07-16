bf843cb54f5012e05de46732419a27d090c4d59a
Dawson Fields <dawson@gofields.com>
2020-07-16 15:53:32 -0500

    Hello

--- src/main/java/com/codechallenge/Bc.java
+++ src/main/java/com/codechallenge/Bc.java
@@ -1,14 +1,1 @@
-package com.codechallenge;
-
-public class Bc {
-
-
-	public static String bC(String number, int sBase, int dBase) {
-		return Integer.toString(Integer.parseInt(number, sBase), dBase);
-	}
-
-	public static char convert(String s) {
-
-		return 'a';
-	}
-}
+package com.codechallenge;public class Bc {public static String bC(String number, int sBase, int dBase) {return Integer.toString(Integer.parseInt(number, sBase), dBase);}public static char convert(String s) {return 'a';}}
