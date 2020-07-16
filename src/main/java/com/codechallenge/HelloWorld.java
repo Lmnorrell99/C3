c641388e9195cf3ebb014379d5205a12dcc1499e
Ryan <ryan@ensignconsultants.com>
2020-07-16 15:17:07 -0500

    trying stuf

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -4,8 +4,20 @@ package com.codechallenge;
  * Created by merciersj on 8/29/2018.
  */
 public class HelloWorld {
+
+	public final String H = "H";
+	public final String E = "E";
+	public final String L = "L";
+	public final String O = "O";
+	public final String W = "W";
+	public final String R = "R";
+	public final String D = "D";
+
+
+
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+
 
 	}
 }
