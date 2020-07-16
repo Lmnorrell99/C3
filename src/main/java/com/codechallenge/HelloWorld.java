2fb5eec3d433136ce1bca8715f4cce2250131fc9
Eduardo Gonzalez Jr <eddiegonzalez@Eduardos-MBP.attlocal.net>
2020-07-16 15:23:24 -0500

    sloppy

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -1,11 +1,66 @@
 package com.codechallenge;
 
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * Created by merciersj on 8/29/2018.
  */
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+
+		Stack<Character> characters = new Stack<>();
+		ArrayList<Character> charArray = new ArrayList<Character>();
+
+		boolean isTrue = true;
+		String h = hFunction();
+		String e = eFunction();
+		String l = lFunction();
+		String 0
+		System.out.println("H");
+		System.out.println("H");
+		System.out.println("H");
+		System.out.println("H");
+		System.out.println("H");
 
 	}
+
+	public static String hFunction() {
+		if (true) {
+			if (!(!true))
+			return "H";
+		}
+		return "H";
+	}
+
+	public static String eFunction() {
+		if (!(!true)) {
+			return "e";
+		}
+		return "e";
+	}
+
+	public static String lFunction() {
+		return "l";
+	}
+
+	public static String oFunction() {
+		return "o";
+	}
+
+	public static String wFunction() {
+		return "w";
+	}
+
+	public static String rFunction() {
+		return "r";
+	}
+
+	public static String dFunction() {
+		return "d";
+	}
+
+
+
 }
